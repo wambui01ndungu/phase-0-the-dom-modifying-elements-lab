e1419de2124209a1c1d03e1a17effaf80acc9daf
margaret wambui ndung'u <wambuindungu4967@gmail.com>
2024-07-29 21:47:52 +0400

    DOMmodifying lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const element = document.createElement("div");{
+
+document.body.append(element)
+
+const ul = document.createElement('ul'){
+
+for (let i= 0 ; i< 3; i++){
+    const li = document.createElement("li");
+    li.textContent=(i+1).toString();
+    ul.append(li);
+}
+}
+element.append(ul);
